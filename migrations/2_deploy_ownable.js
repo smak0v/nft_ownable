3e45fac198e73e7edd052a43f1a0b4eb1f215b59
Sergey Makov <makov.sergey.it@gmail.com>
2021-02-27 20:53:41 +0200

    Fixed migrations

--- migrations/2_deploy_ownable.js
+++ migrations/2_deploy_ownable.js
@@ -1,5 +1,8 @@
 const Ownable = artifacts.require("Ownable");
 
 module.exports = async (deployer, _network, accounts) => {
-  deployer.deploy(Ownable, "KT1P9DW7Z4zFsBV67AqP28JhnwttrVgc8i1Z");
+  deployer.deploy(Ownable, {
+    nft_token_address: "KT1P9DW7Z4zFsBV67AqP28JhnwttrVgc8i1Z",
+    owner: "tz1ZZZZZZZZZZZZZZZZZZZZZZZZZZZZNkiRg",
+  });
 };
--- test/Ownable.test.js
+++ test/Ownable.test.js
@@ -6,6 +6,7 @@ contract("Ownable", async () => {
   before("setup", async () => {
     const ownableStorage = {
       nft_token_address: "KT1P9DW7Z4zFsBV67AqP28JhnwttrVgc8i1Z",
+      owner: "tz1ZZZZZZZZZZZZZZZZZZZZZZZZZZZZNkiRg",
     };
 
     instance = await Ownable.new(ownableStorage);
